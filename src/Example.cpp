cd8c16cc5da296d140208de4a290c8d531e67b58
piotr.zemczak <piotr.zemczak@8d09586a-f783-11de-aa3c-59de0406b4f5>
2010-02-10 20:20:15 +0000

    Added testing of Suffix Tree and string.find().

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,103 +1,81 @@
 #include <iostream>
+#include <fstream>
 #include "SuffixTree.h"
 #include "SuffixTreeTypeTraits.h"
-#include <boost/unordered_set.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/range.hpp>
-#include <string>
 #include <cstring>
+#include <time.h>
 using namespace std;
+
+typedef SuffixTreeTemplate<> SuffixTree;
+typedef SuffixTreeTemplate<unsigned char, std::wstring > SuffixTreeW;
+//typedef SuffixTreeTemplate<unsigned long long> SuffixTreeLL;
+//typedef SuffixTreeTemplate<unsigned int> SuffixTreeINT;
+//typedef SuffixTreeTemplate<unsigned short> SuffixTreeSHRT;
+//typedef SuffixTreeTemplate<unsigned char, SuffixTreeTraits<unsigned char, char> > SuffixTree;
+
+void Search(SuffixTree testTree, string testString, const char* word, int count)
+{	
+	clock_t start, end;
+	bool found;
+
+	start = clock();
+	for (int i = 0; i < count; ++i)
+	{
+		found = testTree.isSuffix(word);
+	}
+	end = clock();
+
+  cout << "Wyszukanie \"" << word << "\" " << count << " razy w SuffixTree: " << (((double)end - (double)start)/(double)CLOCKS_PER_SEC)*1000/(double)count << endl;
+
+	start = clock();
+	for (int i = 0; i < count; ++i)
+	{
+		testString.find(word);
+	}
+	end = clock();
+	
+	cout << "Wyszukanie \"" << word << "\" " << count << " razy w string: " << (((double)end - (double)start)/(double)CLOCKS_PER_SEC)*1000/(double)count << endl;
+}
+
+void PerformTest(string testString)
+{
+	SuffixTree testTree(testString);
+
+	const char* test1 = "I przypomniał sobie całe szeregi ludzi obdartych,";
+
+	Search(testTree, testString, test1, 10000);
+
+	const char* test2 = "Rzecz dziwna! przecie miał już ustaloną opinię hojnego filantropa. Członkowie Towarzystwa Dobroczynności we frakach składali mu podziękowania za ofiarę dla wiecznie łaknącej instytucji; hrabina Karolowa we wszystkich salonach opowiadała o pieniądzach, które złożył na jej ochronę; jego służba i subiekci sławili go za podwyższenie im pensji. Ale Wokulskiemu rzeczy te nie sprawiały żadnej przyjemności, tak jak on sam nie przywiązywał do nich żadnej wagi. Rzucał tysiące rubli do kas urzędowych dobroczyńców, ażeby kupić za to rozgłos nie pytając, co się zrobi z pieniędzmi.";
+
+	Search(testTree, testString, test2, 10000);
+
+	const char* test3 = "SuffixTreeI przypomniał sobie całe szeregi ludzi obdartych,";
+
+	Search(testTree, testString, test3, 10000);
+
+	const char* test4 = "SuffixTreeRzecz dziwna! przecie miał już ustaloną opinię hojnego filantropa. Członkowie Towarzystwa Dobroczynności we frakach składali mu podziękowania za ofiarę dla wiecznie łaknącej instytucji; hrabina Karolowa we wszystkich salonach opowiadała o pieniądzach, które złożył na jej ochronę; jego służba i subiekci sławili go za podwyższenie im pensji. Ale Wokulskiemu rzeczy te nie sprawiały żadnej przyjemności, tak jak on sam nie przywiązywał do nich żadnej wagi. Rzucał tysiące rubli do kas urzędowych dobroczyńców, ażeby kupić za to rozgłos nie pytając, co się zrobi z pieniędzmi.";
+
+	Search(testTree, testString, test4, 10000);
+}
 
 int main(int argc, char** argv)
 {
-	//typedef SuffixTreeTemplate<unsigned long long> SuffixTreeLL;
-	//typedef SuffixTreeTemplate<unsigned int> SuffixTreeINT;
-	//typedef SuffixTreeTemplate<unsigned short> SuffixTreeSHRT;
-	//typedef SuffixTreeTemplate<unsigned char, SuffixTreeTraits<unsigned char, char> > SuffixTree;
-	typedef SuffixTreeTemplate<> SuffixTree;
-	typedef SuffixTreeTemplate<unsigned char, std::wstring > SuffixTreeW;
-	const char* text = "BOOKKE";
-	std::string str(text);
-	std::wstring wstr(L"BOOKKEĘĄŚź");
+	cout << "-----" << endl;
+	cout << "Test wydajnosci." << endl << endl;
+	cout << "-----" << endl;
 
+	string testString;
+	string readString;
 
-
-	std::cout << "Tree uchar: \n";
-	SuffixTree tree( str );
-
-	for (SuffixTree::const_iterator cit = tree.begin(); cit!= tree.end(); ++cit)
+  ifstream fin("lalka.txt");
+	while (getline(fin, readString))
 	{
-		std::cout << *cit << std::endl;
-
+		testString += readString;
 	}
 
-	for (SuffixTree::iterator cit = tree.begin(); cit!= tree.end(); ++cit)
-	{
-		std::cout << *cit << std::endl;
+	cout << "Wczytanie pliku zakonczone" << endl;
 
-	}
+	PerformTest(testString);
 
-	SuffixTreeW wtree( wstr );
-	std::wcout << std::wstring(L"asdźćłżęąń\n");
-
-/*	std::cout << "\nTree ushort: \n";
-	SuffixTreeSHRT treeShrt( str );
-
-	std::cout << "\nTree uint: \n";
-	SuffixTreeINT treeInt( str );
-
-
-
-	std::cout << "\nTree long long: \n";
-	SuffixTreeLL treeLL( str );
-*/
-
-	//typedef SuffixTreeTemplate<unsigned char, SuffixTreeTraits<unsigned char, wchar_t> > WTree;
-
-
-
-	tree.dumpEdges(str.length());
-
-	for (unsigned int i = 0 ; i < str.length(); ++i)
-	{
-		if (!tree.isSuffix(str.substr(i)))
-		{
-			std::cout << "Drzewo jest do dupy" << std::endl;
-		}
-	}
-
-
-	std::cout << "Looks ok"<<std::endl;
-
-
-	if (tree.isSuffix("BOOKKE"))
-		{
-		std::cout << std::endl <<"YES it is"<<std::endl;
-		}
-	else
-	{
-		std::cout << std::endl <<"NO it is not"<<std::endl;
-	}
-
-	std::string s("BADtextBASS");
-	if (tree.isSuffix(s))
-		{
-		std::cout << std::endl <<"YES it is"<<std::endl;
-		}
-	else
-	{
-		std::cout << std::endl <<"NO it is not"<<std::endl;
-	}
-
-
-	if (tree.isSuffix("KK"))
-		{
-		std::cout << std::endl <<"YES it is"<<std::endl;
-		}
-	else
-	{
-		std::cout << std::endl <<"NO it is not"<<std::endl;
-	}
 	return 0;
-
 }
